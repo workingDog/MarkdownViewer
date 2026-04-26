2e9df5d2b2858fa5239520989dd7c366f5fbb42c
workingdog <ringowathelet@gmail.com>
2026-04-26 13:15:00 +0900

    Initial Commit

---
+++ MarkdownViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarkdownViewer
+//
+//  Created by Ringo Wathelet on 2026/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarkdownViewer/MarkdownViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownViewerApp.swift
+//  MarkdownViewer
+//
+//  Created by Ringo Wathelet on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
